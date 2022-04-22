1516e91f16ef5e51f60303d532d027366be03be5
MoralesEspina <60724084+MoralesEspina@users.noreply.github.com>
2022-04-21 23:28:31 -0600

    Xframe

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const xFrameOptions = require('x-frame-options')
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.json());
 app.use(cors());
+app.use(xFrameOptions());
 
 app.use(require('./routes/persona'));
 app.use(require('./routes/docente'));
